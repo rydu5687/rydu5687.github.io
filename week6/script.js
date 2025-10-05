35788756dc1e4f0ca0fb8a82595b3ccf6d32b64b
rydu5687 <rydu5687@colorado.edu>
2025-10-05 13:32:15 -0600

    Barbell comments push

--- week6/script.js
+++ week6/script.js
@@ -1,4 +1,4 @@
-// ===== GALLERY FILTER =====
+// Gallery
 const filterButtons = document.querySelectorAll('.gallery-nav button');
 const photoCards = document.querySelectorAll('.photo-card');
 
@@ -19,7 +19,7 @@ function filterPhotos(category) {
   });
 }
 
-// ===== SEARCH SYSTEM =====
+// Search bar
 const searchInput = document.querySelector('#searchInput');
 const searchButton = document.querySelector('#searchButton');
 
